238a5e09905cbb4e96ef19ad866279b995e05cb9
shohjahon <shohjahonobruyev3@gmail.com>
2024-09-05 21:37:28 +0500

    settings added bloked content setting

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -100,6 +100,18 @@ CORS_ALLOW_HEADERS = (
     "x-csrftoken",
     "x-requested-with",
 )
+
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('X-FORWARDED-PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
